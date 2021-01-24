aebf29b1be2fabdfdbfe5e0a5d1d31f073ae158d
luamfb <luamfb@riseup.net>
2021-01-23 23:04:07 -0300

    Remove command default_orientation

--- include/sway/commands.h
+++ include/sway/commands.h
@@ -119,7 +119,6 @@ sway_cmd cmd_commands;
 sway_cmd cmd_create_output;
 sway_cmd cmd_default_border;
 sway_cmd cmd_default_floating_border;
-sway_cmd cmd_default_orientation;
 sway_cmd cmd_exec;
 sway_cmd cmd_exec_always;
 sway_cmd cmd_exit;
