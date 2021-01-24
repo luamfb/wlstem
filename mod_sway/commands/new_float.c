645825d2596d17ab8c253c61a54580a32894d735
luamfb <luamfb@riseup.net>
2021-01-24 00:26:31 -0300

    Remove border-related commands

--- mod_sway/commands/new_float.c
+++
@@ -1,9 +0,0 @@
-#include "log.h"
-#include "sway/commands.h"
-#include "sway/config.h"
-
-struct cmd_results *cmd_new_float(int argc, char **argv) {
-	sway_log(SWAY_INFO, "Warning: new_float is deprecated. "
-		"Use default_floating_border instead.");
-	return cmd_default_floating_border(argc, argv);
-}
