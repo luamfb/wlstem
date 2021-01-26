1ae7d7029cef157c540ce7f5bec709ffe3d024b5
luamfb <luamfb@riseup.net>
2021-01-26 18:02:41 -0300

    Remove command `no_focus`

--- mod_sway/commands.c
+++ mod_sway/commands.c
@@ -58,7 +58,6 @@ static struct cmd_handler handlers[] = {
 	{ "gaps", cmd_gaps },
 	{ "input", cmd_input },
 	{ "mode", cmd_mode },
-	{ "no_focus", cmd_no_focus },
 	{ "output", cmd_output },
 	{ "seat", cmd_seat },
 	{ "set", cmd_set },
