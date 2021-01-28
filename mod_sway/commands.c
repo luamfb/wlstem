c620aacbdea1c67c3afe2061ce52bd60514af75d
luamfb <luamfb@riseup.net>
2021-01-28 20:23:35 -0300

    Remove cmd_results_to_json()

--- mod_sway/commands.c
+++ mod_sway/commands.c
@@ -5,7 +5,6 @@
 #include <string.h>
 #include <strings.h>
 #include <stdio.h>
-#include <json.h>
 #include "sway/commands.h"
 #include "sway/config.h"
 #include "sway/input/input-manager.h"
@@ -359,25 +358,4 @@ void free_cmd_results(struct cmd_results *results) {
 		free(results->error);
 	}
 	free(results);
-}
-
-char *cmd_results_to_json(list_t *res_list) {
-	json_object *result_array = json_object_new_array();
-	for (int i = 0; i < res_list->length; ++i) {
-		struct cmd_results *results = res_list->items[i];
-		json_object *root = json_object_new_object();
-		json_object_object_add(root, "success",
-				json_object_new_boolean(results->status == CMD_SUCCESS));
-		if (results->error) {
-			json_object_object_add(root, "parse_error",
-					json_object_new_boolean(results->status == CMD_INVALID));
-			json_object_object_add(
-					root, "error", json_object_new_string(results->error));
-		}
-		json_object_array_add(result_array, root);
-	}
-	const char *json = json_object_to_json_string(result_array);
-	char *res = strdup(json);
-	json_object_put(result_array);
-	return res;
 }
