5ffc66edd73e130389812f5e57d802acb4531f67
luamfb <luamfb@riseup.net>
2021-01-30 01:46:48 -0300

    commands.h: Remove unused types
    That also happens to be most of them.

--- include/sway/commands.h
+++ include/sway/commands.h
@@ -4,39 +4,6 @@
 #include <wlr/util/edges.h>
 #include "config.h"
 
-struct sway_container;
-
-typedef struct cmd_results *sway_cmd(int argc, char **argv);
-
-struct cmd_handler {
-    char *command;
-    sway_cmd *handle;
-};
-
-/**
- * Indicates the result of a command's execution.
- */
-enum cmd_status {
-    CMD_SUCCESS,         /**< The command was successful */
-    CMD_FAILURE,        /**< The command resulted in an error */
-    CMD_INVALID,         /**< Unknown command or parser error */
-    CMD_DEFER,        /**< Command execution deferred */
-    CMD_BLOCK,
-    CMD_BLOCK_COMMANDS,
-    CMD_BLOCK_END
-};
-
-/**
- * Stores the result of executing a command.
- */
-struct cmd_results {
-    enum cmd_status status;
-    /**
-     * Human friendly error message, or NULL on success
-     */
-    char *error;
-};
-
 enum expected_args {
     EXPECTED_AT_LEAST,
     EXPECTED_AT_MOST,
