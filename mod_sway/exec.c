6b05cb7cc0444eabd789f31cf87a87a1827a1ea5
luamfb <luamfb@riseup.net>
2021-01-30 23:13:43 -0300

    Move files away from commands folder

---
+++ mod_sway/exec.c
@@ -0,0 +1,68 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <signal.h>
+#include "sway/commands.h"
+#include "sway/config.h"
+#include "sway/tree/container.h"
+#include "sway/tree/root.h"
+#include "sway/tree/workspace.h"
+#include "log.h"
+#include "stringop.h"
+
+bool wls_try_exec(char *cmd) {
+    if (cmd == NULL) {
+        return false;
+    }
+
+    int fd[2];
+    if (pipe(fd) != 0) {
+        sway_log(SWAY_ERROR, "Unable to create pipe for fork");
+    }
+
+    pid_t pid, child;
+    // Fork process
+    if ((pid = fork()) == 0) {
+        // Fork child process again
+        setsid();
+        sigset_t set;
+        sigemptyset(&set);
+        sigprocmask(SIG_SETMASK, &set, NULL);
+        close(fd[0]);
+        if ((child = fork()) == 0) {
+            close(fd[1]);
+            execl("/bin/sh", "/bin/sh", "-c", cmd, (void *)NULL);
+            _exit(0);
+        }
+        ssize_t s = 0;
+        while ((size_t)s < sizeof(pid_t)) {
+            s += write(fd[1], ((uint8_t *)&child) + s, sizeof(pid_t) - s);
+        }
+        close(fd[1]);
+        _exit(0); // Close child process
+    } else if (pid < 0) {
+        close(fd[0]);
+        close(fd[1]);
+        sway_log(SWAY_ERROR, "First fork() failed");
+        return false;
+    }
+    close(fd[1]); // close write
+    ssize_t s = 0;
+    while ((size_t)s < sizeof(pid_t)) {
+        s += read(fd[0], ((uint8_t *)&child) + s, sizeof(pid_t) - s);
+    }
+    close(fd[0]);
+    // cleanup child process
+    waitpid(pid, NULL, 0);
+    if (child > 0) {
+        sway_log(SWAY_DEBUG, "Child process created with pid %d", child);
+        root_record_workspace_pid(child);
+    } else {
+        sway_log(SWAY_ERROR, "Second fork() failed!");
+        return false;
+    }
+    return true;
+}
