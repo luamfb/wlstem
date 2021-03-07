3f9d0df5c26e1ab3d536f60eed94c401b745c2e4
luamfb <luamfb@riseup.net>
2021-03-07 16:12:32 -0300

    output.c: Refuse to add workspaces tied to a different output

--- mod_sway/tree/output.c
+++ mod_sway/tree/output.c
@@ -237,7 +237,9 @@ struct sway_output *output_get_in_direction(struct sway_output *reference,
 void output_add_workspace(struct sway_output *output,
         struct sway_workspace *workspace) {
     if (workspace->output) {
-        workspace_detach(workspace);
+        sway_log(SWAY_ERROR,
+            "tried to add workspace which is already tied to another output");
+        assert(false);
     }
     if (output->active_workspace) {
         sway_log(SWAY_ERROR,
