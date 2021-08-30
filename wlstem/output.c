bcc12068c88a63fc862680aec0fcdeee2554b325
luamfb <luamfb@riseup.net>
2021-08-30 18:04:36 -0300

    Temporarily move some calls away from output_enable()
    These calls actually belong there, but we intend to migrate
    output_enable() to wlstem, and these functions being called are not
    there yet. Therefore, move these calls away for now.

---
+++ wlstem/output.c
@@ -0,0 +1,24 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <wayland-server-core.h>
+#include "list.h"
+#include "log.h"
+#include "output.h"
+#include "wlstem.h"
+
+void output_enable(struct sway_output *output) {
+    if (!sway_assert(!output->enabled, "output is already enabled")) {
+        return;
+    }
+    output->tiling = create_list();
+    output->enabled = true;
+    list_add(wls->root->outputs, output);
+
+    if (!output->active) {
+        sway_log(SWAY_DEBUG, "Activating output '%s'", output->wlr_output->name);
+        output->active = true;
+    }
+
+    wl_signal_emit(&wls->node_manager->events.new_node, &output->node);
+    wl_signal_emit(&wls->root->events.output_connected, output);
+}
