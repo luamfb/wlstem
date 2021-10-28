e0d9775500914bf64465311d35dbe8f7a84f3e28
luamfb <luamfb@riseup.net>
2021-10-28 01:58:45 -0300

    Move input_config definition to new header in wlstem

--- include/sway_config.h
+++ include/sway_config.h
@@ -11,6 +11,7 @@
 #include "../include/config.h"
 #include "list.h"
 #include "container.h"
+#include "input_config.h"
 #include "output_config.h"
 #include "tablet.h"
 #include "output_manager.h"
@@ -53,73 +54,6 @@ struct sway_mode {
     list_t *keysym_bindings;
     list_t *keycode_bindings;
     bool pango;
-};
-
-struct input_config_mapped_from_region {
-    double x1, y1;
-    double x2, y2;
-    bool mm;
-};
-
-struct calibration_matrix {
-    bool configured;
-    float matrix[6];
-};
-
-enum input_config_mapped_to {
-    MAPPED_TO_DEFAULT,
-    MAPPED_TO_OUTPUT,
-    MAPPED_TO_REGION,
-};
-
-/**
- * options for input devices
- */
-struct input_config {
-    char *identifier;
-    const char *input_type;
-
-    int accel_profile;
-    struct calibration_matrix calibration_matrix;
-    int click_method;
-    int drag;
-    int drag_lock;
-    int dwt;
-    int left_handed;
-    int middle_emulation;
-    int natural_scroll;
-    float pointer_accel;
-    float scroll_factor;
-    int repeat_delay;
-    int repeat_rate;
-    int scroll_button;
-    int scroll_method;
-    int send_events;
-    int tap;
-    int tap_button_map;
-
-    char *xkb_layout;
-    char *xkb_model;
-    char *xkb_options;
-    char *xkb_rules;
-    char *xkb_variant;
-    char *xkb_file;
-
-    bool xkb_file_is_set;
-
-    int xkb_numlock;
-    int xkb_capslock;
-
-    struct input_config_mapped_from_region *mapped_from_region;
-
-    enum input_config_mapped_to mapped_to;
-    char *mapped_to_output;
-    struct wlr_box *mapped_to_region;
-
-    list_t *tools;
-
-    bool capturable;
-    struct wlr_box region;
 };
 
 /**
