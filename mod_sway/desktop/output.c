68a43ce493b6a4d305d10e2443629df9db35c649
luamfb <luamfb@riseup.net>
2021-10-31 09:00:45 -0300

    Migrate useful output-related functions to wlstem

--- mod_sway/desktop/output.c
+++ mod_sway/desktop/output.c
@@ -32,32 +32,6 @@
 #include "wlstem.h"
 #include "server.h"
 
-struct sway_output *output_by_name_or_id(const char *name_or_id) {
-    for (int i = 0; i < wls->output_manager->outputs->length; ++i) {
-        struct sway_output *output = wls->output_manager->outputs->items[i];
-        char identifier[128];
-        output_get_identifier(identifier, sizeof(identifier), output);
-        if (strcasecmp(identifier, name_or_id) == 0
-                || strcasecmp(output->wlr_output->name, name_or_id) == 0) {
-            return output;
-        }
-    }
-    return NULL;
-}
-
-struct sway_output *all_output_by_name_or_id(const char *name_or_id) {
-    struct sway_output *output;
-    wl_list_for_each(output, &wls->output_manager->all_outputs, link) {
-        char identifier[128];
-        output_get_identifier(identifier, sizeof(identifier), output);
-        if (strcasecmp(identifier, name_or_id) == 0
-                || strcasecmp(output->wlr_output->name, name_or_id) == 0) {
-            return output;
-        }
-    }
-    return NULL;
-}
-
 static void update_textures(struct sway_container *con, void *data) {
     container_update_title_textures(con);
 }
@@ -88,22 +62,4 @@ void handle_output_layout_change(struct wl_listener *listener,
         wl_container_of(listener, server, output_layout_change);
     wls_update_output_manager_config(wls->output_manager);
     wl_signal_emit(&wls->output_manager->events.output_layout_changed, wls->output_manager);
-}
-
-void handle_output_power_manager_set_mode(struct wl_listener *listener,
-        void *data) {
-    struct wlr_output_power_v1_set_mode_event *event = data;
-    struct sway_output *output = event->output->data;
-
-    struct output_config *oc = new_output_config(output->wlr_output->name);
-    switch (event->mode) {
-    case ZWLR_OUTPUT_POWER_V1_MODE_OFF:
-        oc->dpms_state = DPMS_OFF;
-        break;
-    case ZWLR_OUTPUT_POWER_V1_MODE_ON:
-        oc->dpms_state = DPMS_ON;
-        break;
-    }
-    oc = store_output_config(oc);
-    apply_output_config(oc, output);
 }
--- mod_sway/tree/output.c
+++ mod_sway/tree/output.c
@@ -12,12 +12,6 @@
 #include "util.h"
 #include "wlstem.h"
 
-void seize_containers_from_noop_output(struct sway_output *output) {
-    if (wls->output_manager->noop_output->active) {
-        output_seize_containers_from(output, wls->output_manager->noop_output);
-    }
-}
-
 struct sway_output * choose_absorber_output(struct sway_output *giver) {
     struct sway_output *absorber = NULL;
     if (wls->output_manager->outputs->length > 1) {
@@ -27,32 +21,4 @@ struct sway_output * choose_absorber_output(struct sway_output *giver) {
         }
     }
     return absorber;
-}
-
-struct sway_output *output_from_wlr_output(struct wlr_output *output) {
-    return output->data;
-}
-
-struct sway_output *output_get_in_direction(struct sway_output *reference,
-        enum wlr_direction direction) {
-    if (!sway_assert(direction, "got invalid direction: %d", direction)) {
-        return NULL;
-    }
-    struct wlr_box *output_box =
-        wlr_output_layout_get_box(wls->output_manager->output_layout, reference->wlr_output);
-    int lx = output_box->x + output_box->width / 2;
-    int ly = output_box->y + output_box->height / 2;
-    struct wlr_output *wlr_adjacent = wlr_output_layout_adjacent_output(
-            wls->output_manager->output_layout, direction, reference->wlr_output, lx, ly);
-    if (!wlr_adjacent) {
-        return NULL;
-    }
-    return output_from_wlr_output(wlr_adjacent);
-}
-
-void output_get_render_box(struct sway_output *output, struct wlr_box *box) {
-    box->x = output->render_lx;
-    box->y = output->render_ly;
-    box->width = output->usable_area.width;
-    box->height = output->usable_area.height;
 }
