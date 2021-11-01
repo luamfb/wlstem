049c74a2642868973a82391af28bf3bf6c6c78cc
luamfb <luamfb@riseup.net>
2021-11-01 06:15:34 -0300

    Migrate several functions from tree/container.c to wlstem

--- wlstem/at.c
+++ wlstem/at.c
@@ -39,3 +39,58 @@ struct sway_container *surface_at_view(struct sway_container *con, double lx, do
     }
     return NULL;
 }
+
+struct sway_container *view_container_at(struct wls_transaction_node *parent,
+        double lx, double ly,
+        struct wlr_surface **surface, double *sx, double *sy) {
+    if (!sway_assert(node_is_view(parent), "Expected a view")) {
+        return NULL;
+    }
+
+    struct sway_container *container = parent->sway_container;
+    struct wlr_box box = {
+            .x = container->x,
+            .y = container->y,
+            .width = container->width,
+            .height = container->height,
+    };
+
+    if (wlr_box_contains_point(&box, lx, ly)) {
+        surface_at_view(parent->sway_container, lx, ly, surface, sx, sy);
+        return container;
+    }
+
+    return NULL;
+}
+
+static struct sway_container *toplevel_window_at_recurse(struct wls_transaction_node *parent,
+        double lx, double ly,
+        struct wlr_surface **surface, double *sx, double *sy) {
+    list_t *children = node_get_children(parent);
+    if (children) {
+        for (int i = 0; i < children->length; ++i) {
+            struct sway_container *child = children->items[i];
+            struct sway_container *container =
+                toplevel_window_at(&child->node, lx, ly, surface, sx, sy);
+            if (container) {
+                return container;
+            }
+        }
+    }
+    return NULL;
+}
+
+struct sway_container *toplevel_window_at(struct wls_transaction_node *parent,
+        double lx, double ly,
+        struct wlr_surface **surface, double *sx, double *sy) {
+    if (node_is_view(parent)) {
+        return view_container_at(parent, lx, ly, surface, sx, sy);
+    }
+    if (!node_get_children(parent)) {
+        return NULL;
+    }
+    if (node_may_have_container_children(parent)) {
+        return toplevel_window_at_recurse(parent, lx, ly, surface, sx, sy);
+    }
+    return NULL;
+}
